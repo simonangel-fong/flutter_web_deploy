9686feb04ae7c5c1a95cae01f2a1b1afde3a7cbc
simonangel-fong <simonangelfong@gmail.com>
2024-03-22 16:18:28 -0400

    test/cicd

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page - Test for CICD'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page - Test for CICD Tag: 1.0.0'),
     );
   }
 }
